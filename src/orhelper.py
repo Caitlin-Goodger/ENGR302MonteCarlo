6949a4af89eb1d47e2f6d93d925bd7cdb7716cfe
DurantZac <zdurantnz@gmail.com>
2020-05-08 16:01:04 +1200

    Fix malformed statement

--- src/orhelper.py
+++ src/orhelper.py
@@ -33,8 +33,7 @@ class OpenRocketInstance(object):
         if not ty is None:
             print ('Exception while calling openrocket')
             print ('Exception info : ', ty, value, tb)
-            print ('Traceback : '
-            traceback.print_exception(ty, value, tb))
+            print ('Traceback : ',traceback.print_exception(ty, value, tb))
         
 class Helper(object):
     """ This class contains a variety of useful helper functions and wrapper for using
