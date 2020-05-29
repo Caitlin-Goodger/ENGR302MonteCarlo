90ff57040f2f29239f834b622c1ab6b65580de7d
DurantZac <zdurantnz@gmail.com>
2020-05-29 15:46:32 +1200

    Test invalid arguments and all defaults

--- src/test_args.py
+++ src/test_args.py
@@ -15,9 +15,20 @@ def test_no_params():
         args = 0
         sim.set_args(args)
         sim.parse_args()
-        assert sim.args.outfile == "./out.csv" and sim.args.rocket == "model.ork" and sim.args.simcount == 20
+        assert sim.args.outfile == "./out.csv" 
+        assert sim.args.rocket == "model.ork" 
+        assert sim.args.simcount == 20  
+        assert sim.args.rodangle == 45 
+        assert sim.args.rodanglesigma == 5 
+        assert sim.args.roddirection == 0 
+        assert sim.args.roddirectionsigma == 5
+        assert sim.args.windspeed == 15
+        assert sim.args.windspeedsigma == 5
+        assert sim.args.startlat == 0
+        assert sim.args.startlong == 0
 
-# Test the user is given overview on available parameters
+
+# Test the user is given overview on available parameters when using help flag
 def test_help(capsys):
     try:
         from unittest.mock import patch
@@ -33,3 +44,21 @@ def test_help(capsys):
             sim.parse_args()
             out = capsys.readouterr()
             assert "optional arguments:" in out.out
+
+# Test the user is given overview on available parameters when using invalid flag
+def test_invalid(capsys):
+    try:
+        from unittest.mock import patch
+    except ImportError:
+        from mock import patch
+
+    testargs = ["prog","-invalid"]
+    with patch.object(sys, 'argv', testargs):  
+        with patch.object(sys, "exit"):
+            sim = simulation.Simulation()
+            args = 0
+            sim.set_args(args)
+            sim.parse_args()
+            out,err = capsys.readouterr()
+            assert "optional arguments:" in out
+            assert "error: unrecognized arguments" in err
