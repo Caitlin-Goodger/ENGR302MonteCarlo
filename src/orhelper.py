0484bf6c358b15dfc9446033a3cc318f4bcea880
Zak Kiernander <kiernazak@ecs.vuw.ac.nz>
2020-06-05 14:46:16 +1200

    Disable 3d

--- src/orhelper.py
+++ src/orhelper.py
@@ -16,7 +16,7 @@ class OpenRocketInstance(object):
 
         print("Startup")
             
-        startJVM(getDefaultJVMPath(), "-Djava.class.path=%s" % jar_path, convertStrings=False)
+        startJVM(getDefaultJVMPath(), "-Djava.class.path=%s" % jar_path,"-Dopenrocket.3d.disable=true", convertStrings=False)
 
         orp = JPackage("net").sf.openrocket
         orp.startup.Startup2.loadMotor()
