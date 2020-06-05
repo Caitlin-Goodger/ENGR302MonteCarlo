744a96f3e067c265bae333cbbc1481d2d370a2c5
nstallinger <nicstallinger@gmail.com>
2020-06-05 15:45:04 +1200

    Worked on parsing args from GUI to simulation. Currently broken. Broken.

--- src/gui.py
+++ src/gui.py
@@ -1,50 +1,62 @@
 import tkinter as tk 
+import simulation 
 from tkinter import filedialog
+from argparse import Namespace
 r = tk.Tk() 
 r.title('Loader') 
 
 class Window:
     def __init__(self, master):
+        self.filename = './out.csv'
         tk.Button(master, text='Open Rocket', width=25, command=self.getFile).pack()
         
         vnumber = (master.register(self.validateNumber),'%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 
         #rda
         tk.Label(master, text="Rod angle").pack()
-        self.rodAngle=tk.StringVar()
-        rodAngleInp=tk.Entry(master,width=25,textvariable=self.rodAngle,validate="key",validatecommand=vnumber).pack()
+        self.rodangleInp=tk.StringVar()
+        self.rodangle=tk.Entry(master,width=25,textvariable=self.rodangleInp,validate="key",validatecommand=vnumber)
+        self.rodangle.pack()
         # rdas
         tk.Label(master, text="rod angle sigma").pack()
         self.rodanglesigmaInp=tk.StringVar()
-        rodanglesigma=tk.Entry(master,width=25,textvariable=self.rodanglesigmaInp,validate="key",validatecommand=vnumber).pack()
+        self.rodanglesigma=tk.Entry(master,width=25,textvariable=self.rodanglesigmaInp,validate="key",validatecommand=vnumber)
+        self.rodanglesigma.pack()
         # rdd
         tk.Label(master, text="rod direction").pack()
         self.roddirectionInp=tk.StringVar()
-        roddirection=tk.Entry(master,width=25,textvariable=self.roddirectionInp,validate="key",validatecommand=vnumber).pack()
+        self.roddirection=tk.Entry(master,width=25,textvariable=self.roddirectionInp,validate="key",validatecommand=vnumber)
+        self.roddirection.pack()
         # rdds
         tk.Label(master, text="rod direction sigma").pack()
         self.roddirectionsigmaInp=tk.StringVar()
-        roddirectionsigma=tk.Entry(master,width=25,textvariable=self.roddirectionsigmaInp,validate="key",validatecommand=vnumber).pack()
+        self.roddirectionsigma=tk.Entry(master,width=25,textvariable=self.roddirectionsigmaInp,validate="key",validatecommand=vnumber)
+        self.roddirectionsigma.pack()
         # wsa
         tk.Label(master, text="wind speed").pack()
         self.windspeedInp=tk.StringVar()
-        windspeed=tk.Entry(master,width=25,textvariable=self.windspeedInp,validate="key",validatecommand=vnumber).pack()
+        self.windspeed=tk.Entry(master,width=25,textvariable=self.windspeedInp,validate="key",validatecommand=vnumber)
+        self.windspeed.pack()
         # wsas
         tk.Label(master, text="wind speed sigma").pack()
         self.windspeedsigmaInp=tk.StringVar()
-        windspeedsigma=tk.Entry(master,width=25,textvariable=self.windspeedsigmaInp,validate="key",validatecommand=vnumber).pack()
+        self.windspeedsigma=tk.Entry(master,width=25,textvariable=self.windspeedsigmaInp,validate="key",validatecommand=vnumber)
+        self.windspeedsigma.pack()
         # lat
-        tk.Label(master, text="lat").pack()
-        self.latInp=tk.StringVar()
-        lat=tk.Entry(master,width=25,textvariable=self.latInp,validate="key",validatecommand=vnumber).pack()
+        tk.Label(master, text="lat").pack() 
+        self.latInp=tk.DoubleVar()
+        self.lat=tk.Entry(master,width=25,textvariable=self.latInp,validate="key",validatecommand=vnumber)
+        self.lat.pack()
         # long
         tk.Label(master, text="long").pack()
         self.longaInp=tk.StringVar()
-        longa=tk.Entry(master,width=25,textvariable=self.longaInp,validate="key",validatecommand=vnumber).pack()
+        self.longa=tk.Entry(master,width=25,textvariable=self.longaInp,validate="key",validatecommand=vnumber)
+        self.longa.pack()
         # n
         tk.Label(master, text="Number of iteration").pack()
         self.nInp=tk.StringVar()
-        n=tk.Entry(master,width=25,textvariable=self.nInp,validate="key",validatecommand=vnumber).pack()
+        self.n=tk.Entry(master,width=25,textvariable=self.nInp,validate="key",validatecommand=vnumber)
+        self.n.pack()
 
         tk.Button(master, text='Execute', width=25, command=self.exec).pack()
 
@@ -57,7 +69,33 @@ class Window:
         return S.isdigit()
 
     def exec(self):
-        print(self.rodAngle.get())
+        # print(self.rodAngle.get())
+        args = Namespace(outfile='./out.csv', rocket='model.ork', rodangle=45, rodanglesigma=5, roddirection=0, roddirectionsigma=5, simcount=20, startlat=0, startlong=0, windspeed=15)
+        
+        if self.filename != '':
+            args.rocket = self.filename
+        if self.rodangle.get() != '':
+            args.rodangle = float(self.rodangle.get())
+        if self.rodanglesigma.get() != '':
+            args.rodanglesigma = float(self.rodanglesigma.get())
+        if self.roddirection.get() != '':
+            args.roddirection = float(self.roddirection.get())
+        if self.roddirectionsigma.get() != '':
+            args.roddirectionsigma = float(self.roddirectionsigma.get())
+        if self.nInp.get() != '':
+            args.simcount = float(self.nInp.get())
+        if self.lat.get() != '':
+            args.startlat = float(self.lat.get())
+        if self.longa.get() != '':
+            args.startlong = float(self.longa.get())
+        if self.windspeed.get() != '':
+            args.windspeed = float(self.windspeed.get())
+        
+        print(args)
+        sim = simulation.Simulation()
+        sim.set_args(args)
+        #sim.parse_args()
+        sim.runSimulation()
 
 
 window=Window(r)
