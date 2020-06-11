ceaa13e8af7f0053017673bf53b738ecbfa22898
nstallinger <nicstallinger@gmail.com>
2020-06-11 12:57:20 +1200

    Refactored gui.py into a main app class with windows. After running the simulation, the gui switches to a new window.

--- src/gui.py
+++ src/gui.py
@@ -2,61 +2,88 @@ import tkinter as tk
 import simulation 
 from tkinter import filedialog
 from argparse import Namespace
-r = tk.Tk() 
-r.title('Loader') 
 
-class Window:
-    def __init__(self, master):
+class MonteCarloApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        #App window size
+        self.geometry("500x500")
+        self.title('Loader')
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        #Layer frames on top of each other. The top of the stack order is the visible page.
+        self.frames = {}
+        for F in (InputOptions, RunningSimulations):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("InputOptions")
+
+    #Switch which frame is visible
+    def show_frame(self, page_name):
+        #Show a frame for the given page name
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+class InputOptions(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
         self.filename = './out.csv'
-        tk.Button(master, text='Open Rocket', width=25, command=self.getFile).pack()
+        tk.Button(self, text='Open Rocket', width=25, command=self.getFile).grid(column=0, row=0)
 
         #rda
-        tk.Label(master, text="Rod angle").pack()
+        tk.Label(self, text="Rod angle").grid(column=0, row=1)
         self.rodangleInp=tk.StringVar()
-        self.rodangle=tk.Entry(master,width=25,textvariable=self.rodangleInp)
-        self.rodangle.pack()
+        self.rodangle=tk.Entry(self,width=25,textvariable=self.rodangleInp)
+        self.rodangle.grid(column=0, row=2)
         # rdas
-        tk.Label(master, text="rod angle sigma").pack()
+        tk.Label(self, text="rod angle sigma").grid(column=0, row=3)
         self.rodanglesigmaInp=tk.StringVar()
-        self.rodanglesigma=tk.Entry(master,width=25,textvariable=self.rodanglesigmaInp)
-        self.rodanglesigma.pack()
+        self.rodanglesigma=tk.Entry(self,width=25,textvariable=self.rodanglesigmaInp)
+        self.rodanglesigma.grid(column=0, row=4)
         # rdd
-        tk.Label(master, text="rod direction").pack()
+        tk.Label(self, text="rod direction").grid(column=0, row=5)
         self.roddirectionInp=tk.StringVar()
-        self.roddirection=tk.Entry(master,width=25,textvariable=self.roddirectionInp)
-        self.roddirection.pack()
+        self.roddirection=tk.Entry(self,width=25,textvariable=self.roddirectionInp)
+        self.roddirection.grid(column=0, row=6)
         # rdds
-        tk.Label(master, text="rod direction sigma").pack()
+        tk.Label(self, text="rod direction sigma").grid(column=0, row=7)
         self.roddirectionsigmaInp=tk.StringVar()
-        self.roddirectionsigma=tk.Entry(master,width=25,textvariable=self.roddirectionsigmaInp)
-        self.roddirectionsigma.pack()
+        self.roddirectionsigma=tk.Entry(self,width=25,textvariable=self.roddirectionsigmaInp)
+        self.roddirectionsigma.grid(column=0, row=8)
         # wsa
-        tk.Label(master, text="wind speed").pack()
+        tk.Label(self, text="wind speed").grid(column=0, row=9)
         self.windspeedInp=tk.StringVar()
-        self.windspeed=tk.Entry(master,width=25,textvariable=self.windspeedInp)
-        self.windspeed.pack()
+        self.windspeed=tk.Entry(self,width=25,textvariable=self.windspeedInp)
+        self.windspeed.grid(column=0, row=10)
         # wsas
-        tk.Label(master, text="wind speed sigma").pack()
+        tk.Label(self, text="wind speed sigma").grid(column=0, row=11)
         self.windspeedsigmaInp=tk.StringVar()
-        self.windspeedsigma=tk.Entry(master,width=25,textvariable=self.windspeedsigmaInp)
-        self.windspeedsigma.pack()
+        self.windspeedsigma=tk.Entry(self,width=25,textvariable=self.windspeedsigmaInp)
+        self.windspeedsigma.grid(column=0, row=12)
         # lat
-        tk.Label(master, text="lat").pack() 
+        tk.Label(self, text="lat").grid(column=0, row=13)
         self.latInp=tk.DoubleVar()
-        self.lat=tk.Entry(master,width=25,textvariable=self.latInp)
-        self.lat.pack()
+        self.lat=tk.Entry(self,width=25,textvariable=self.latInp)
+        self.lat.grid(column=0, row=14)
         # long
-        tk.Label(master, text="long").pack()
+        tk.Label(self, text="long").grid(column=0, row=15)
         self.longaInp=tk.StringVar()
-        self.longa=tk.Entry(master,width=25,textvariable=self.longaInp)
-        self.longa.pack()
+        self.longa=tk.Entry(self,width=25,textvariable=self.longaInp)
+        self.longa.grid(column=0, row=16)
         # n
-        tk.Label(master, text="Number of iteration").pack()
+        tk.Label(self, text="Number of iteration").grid(column=0, row=17)
         self.nInp=tk.StringVar()
-        self.n=tk.Entry(master,width=25,textvariable=self.nInp)
-        self.n.pack()
+        self.n=tk.Entry(self,width=25,textvariable=self.nInp)
+        self.n.grid(column=0, row=18)
 
-        tk.Button(master, text='Execute', width=25, command=self.exec).pack()
+        tk.Button(self, text='Execute', width=25, command=self.exec).grid(column=0, row=19)
 
     def getFile(self):
         self.filename =  tk.filedialog.askopenfilename(initialdir = "./",title = "Select file",filetypes = [("Rocket File","*.ork")])
@@ -94,7 +121,17 @@ class Window:
         sim.set_args(args)
         #sim.parse_args()
         sim.runSimulation()
+        self.controller.show_frame("RunningSimulations")
 
+class RunningSimulations(tk.Frame):
 
-window=Window(r)
-r.mainloop() 
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("InputOptions"))
+        button.grid(column=0, row=0)
+
+if __name__ == "__main__":
+    app = MonteCarloApp()
+    app.mainloop()
