21f7b702372ceb034f9c94753662c414d6bbf42d
Zak Kiernander <kiernazak@ecs.vuw.ac.nz>
2020-07-31 10:37:11 +1200

    Maps webview
    Co-authored-by: @goodgecait

--- src/maps.py
+++ src/maps.py
@@ -1,28 +1,45 @@
-import numpy as np
+import plotly.graph_objects as go
+import plotly as plotly
 import pandas as pd
-import matplotlib.pyplot as plt
+import webview
 
 df = pd.read_csv("./out.csv")
 df.head()
 
-# BBox = ((np.format_float_positional(df.Longitude.min()), np.format_float_positional(df.Longitude.max()),      
-#          np.format_float_positional(df.Latitude.min()),  np.format_float_positional(df.Latitude.max())))
-# BBox = (174.76557 ,174.78242,-41.28333,-41.29030)
-# BBox =(174.76840,174.77690,-41.28903,-41.28461)
-BBox =(174.76840,174.77690,-41.28903,-41.28461)
+mapbox_access_token = "pk.eyJ1IjoicGxvdGx5LWRvY3MiLCJhIjoiY2s1MnNtODFwMDE4YjNscG5oYWNydXFxYSJ9.AquTxb6AI-oo7TWt01YQ9Q"#open(".mapbox_token").read()
 
-print(BBox)
+fig = go.Figure(go.Scattermapbox(
+        lat=df.Latitude,
+        lon=df.Longitude,
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=9
+        ),
+        # text=["Sim 1"],
+    ))
 
-# http://openstreetmap.org/#map=17/-41.28682/174.77212
-ruh_m = plt.imread("./map.png")
+fig.update_layout(
+    autosize=True,
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=df.Latitude.mean(),
+            lon=df.Longitude.mean()
+        ),
+        pitch=0,
+        zoom=10
+    ),
+)
 
-fig, ax = plt.subplots(figsize = (8,7))
-# fig, ax=plt.subplots()
-ax.scatter(df.Longitude, df.Latitude, zorder=1, alpha= 0.2, c='b', s=10)
-ax.set_title('Plotting Spatial Data')
-ax.set_xlim(BBox[0],BBox[1])
-ax.set_ylim(BBox[2],BBox[3])
-ax.imshow(ruh_m, zorder=0, extent = BBox)#, aspect= 'equal')
-# fig.show(block=True)
-plt.show()
-# input("idk")
+# https://plotly.com/python/renderers/#setting-the-default-renderer
+fig.show(renderer="iframe")
+# conda install -c plotly plotly-orca
+# print(fig.show(renderer="png"))
+# fig.show()
+
+ht = open("./iframe_figures/figure_0.html","r")
+webview.create_window('Hello world', html=ht.read())
+webview.start()
+ht.close()
