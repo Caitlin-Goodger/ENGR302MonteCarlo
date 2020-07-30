2fa3e62750cccb0427633e7a0340ba564a0db8e8
Luisa Kristen <Luisa.Kristen@gmail.com>
2020-07-31 10:12:23 +1200

    Refactor Variable names to be consistent with CSV files

--- src/simulation.py
+++ src/simulation.py
@@ -46,18 +46,18 @@ class Simulation(object):
         parser.add_argument("-rocket", "--rocket", dest = "rocket", default = self.resource_path("model.ork"), help="The model rocket file to run the simulation with.")
         parser.add_argument("-o", "--output", dest = "outfile", default = "./out.csv", help="The output CSV location.")
 
-        parser.add_argument("-rda", "--rodangle", dest = "rodangle", default = 45, help="The rod angle to launch at.", type = float)
-        parser.add_argument("-rdas", "--rodanglesigma", dest = "rodanglesigma", default = 5, help="The rod angle sigma to launch at.", type = float)
+        parser.add_argument("-rda", "--rodangle", dest = "rodAngle", default = 45, help="The rod angle to launch at.", type = float)
+        parser.add_argument("-rdas", "--rodanglesigma", dest = "rodAngleSigma", default = 5, help="The rod angle sigma to launch at.", type = float)
 
-        parser.add_argument("-rdd", "--roddirection", dest = "roddirection", default = 0, help="The rod direction to launch at.", type = float)
-        parser.add_argument("-rdds", "--roddirectionsigma", dest = "roddirectionsigma", default = 5, help="The rod direction sigma to launch at.", type = float)
+        parser.add_argument("-rdd", "--roddirection", dest = "rodDirection", default = 0, help="The rod direction to launch at.", type = float)
+        parser.add_argument("-rdds", "--roddirectionsigma", dest = "rodDirectionSigma", default = 5, help="The rod direction sigma to launch at.", type = float)
 
-        parser.add_argument("-wsa", "--windspeed", dest = "windspeed", default = 15, help="The average wind speed.", type = float)
-        parser.add_argument("-wsas", "--windspeedsigma", dest = "windspeedsigma", default = 5, help="The average wind speed sigma.", type = float)
+        parser.add_argument("-wsa", "--windspeed", dest = "windSpeed", default = 15, help="The average wind speed.", type = float)
+        parser.add_argument("-wsas", "--windspeedsigma", dest = "windSpeedSigma", default = 5, help="The average wind speed sigma.", type = float)
 
-        parser.add_argument("-lat", "--lat", dest = "startlat", default= 0, help = "The starting latitude for the simulation.",  type = float)
-        parser.add_argument("-long", "--long", dest = "startlong", default= 0, help = "The starting longitude for the simulation.",  type = float)
-        parser.add_argument("-n", "--n", dest = "simcount", default = 20, help = "The number of simulations to run.", type = int) 
+        parser.add_argument("-lat", "--lat", dest = "startLat", default= 0, help = "The starting latitude for the simulation.",  type = float)
+        parser.add_argument("-long", "--long", dest = "startLong", default= 0, help = "The starting longitude for the simulation.",  type = float)
+        parser.add_argument("-n", "--n", dest = "simCount", default = 20, help = "The number of simulations to run.", type = int) 
         parser.add_argument("-w", "--w", dest = "weathercsv", default = "", help = "Weather csv file")
 
         self.args = parser.parse_args()
@@ -65,7 +65,7 @@ class Simulation(object):
         return self.points
 
     def runSimulation(self):
-        self.points.add_simulations(self.args.simcount)
+        self.points.add_simulations(self.args.simCount)
         self.points.print_stats()
         return self.points
 
