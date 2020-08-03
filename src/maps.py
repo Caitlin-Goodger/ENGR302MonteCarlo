c6bad9a3abc68f3232a355a81281cddd463e1ed3
Zak Kiernander <kiernazak@ecs.vuw.ac.nz>
2020-08-03 12:08:24 +1200

    Refactor to leaflet
    Massively reduces dependencies and overall render size.
    
      Co-authored-by: @goodgecait

--- src/maps.py
+++ src/maps.py
@@ -2,70 +2,38 @@ import plotly.graph_objects as go
 import plotly as plotly
 import pandas as pd
 import webview
-import os
-import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("./out.csv")
 df.head()
 
-# open(".mapbox_token").read()
+f = open("./iframe_figures/figure_0.html", "w")
+
 mapbox_access_token = "pk.eyJ1IjoicGxvdGx5LWRvY3MiLCJhIjoiY2s1MnNtODFwMDE4YjNscG5oYWNydXFxYSJ9.AquTxb6AI-oo7TWt01YQ9Q"
 
-fig = go.Figure(go.Scattermapbox(
-    lat=df.Latitude,
-    lon=df.Longitude,
-    mode='markers',
-    marker=go.scattermapbox.Marker(
-        size=9
-    ),
-    # text=["Sim 1"],
-))
+markers=""
 
-fig.update_layout(
-    autosize=False,
-    hovermode='closest',
-    width=800,
-    height=800,
-    margin=go.layout.Margin(
-        l=0,
-        r=0,
-        b=0,
-        t=0,
-        pad = 0
-    ),
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        bearing=0,
-        center=dict(
-            lat=df.Latitude.mean(),
-            lon=df.Longitude.mean()
-        ),
-        pitch=0,
-        zoom=17
-    ),
-)
+for index, row in df.iterrows():
+    markers+="L.marker([{lat},{long}]).addTo(map);".format(long=row["Longitude"],lat=row["Latitude"])
+    
+base="""
+<link rel="stylesheet" href="./leaf/leaflet.css" />
+<script src="./leaf/leaflet.js"></script>
+<body style="margin:0px;">
+    <div id="mapid" style="width: 100%;height: 100%;"></div>
+    <script>
+        var map = L.map('mapid').setView([""" +np.format_float_positional(df.Latitude.min())+","+np.format_float_positional(df.Longitude.min())+ """], 17);
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-# https://plotly.com/python/renderers/#setting-the-default-renderer
-fig.show(renderer="iframe")
-# conda install -c plotly plotly-orca
-# print(fig.show(renderer="png"))
-# fig.show()
-f = os.path.isfile('./iframe_figures/figure_0.html')
-print (f)
-if not f:
-    x = df.Longitude
-    y = df.Latitude
-    colors = (0,0,0)
-    area = np.pi*3
+"""+markers+"""
+    </script>    
+</body>    
+"""
+f.write(base)
+f.close()
 
-    # Plot
-    plt.scatter(x, y, s=area, c=colors, alpha=0.5)
-    plt.title('Scatter plot pythonspot.com')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
-if f:
-    ht = open("./iframe_figures/figure_0.html", "r")
-    webview.create_window('Simulations', html=ht.read(),width=840,height=860)
-    webview.start()
-    ht.close()
+webview.create_window('Simulations', url="./iframe_figures/figure_0.html",width=900,height=900)
+webview.start(http_server=True)
