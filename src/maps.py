1d50cd9e4084dff611fcb9b993a92450a8550825
Zak Kiernander <kiernazak@ecs.vuw.ac.nz>
2020-08-14 12:07:58 +1200

    No cache tile proxy
    Further work is needed for caching ( Must remain single threaded to comply with openmap tile server) this in turn makes it extremely slow on first tiles load

--- src/maps.py
+++ src/maps.py
@@ -2,8 +2,9 @@ import pandas as pd
 import webview
 import numpy as np
 from threading import Timer
+import urllib.request
 
-from flask import Flask,send_from_directory
+from flask import Flask,send_from_directory,Response
 flaskServer = Flask(__name__)
 
 df = pd.read_csv("./maps_test.csv")
@@ -26,10 +27,12 @@ base="""
     <script>
         var map = L.map('mapid').setView([""" +np.format_float_positional(df.Latitude.min())+","+np.format_float_positional(df.Longitude.min())+ """], 17);
     
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        L.tileLayer('./tiles/{s}/{z}/{x}/{y}.png', {
 
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+    }).addTo(map);
 """+markers+"""
     </script>    
 </body>    
@@ -40,13 +43,30 @@ f.close()
 
 @flaskServer.route('/')
 def index():
+    print("Base get")
     return base
 @flaskServer.route('/leaf/<path:path>')
 def leafServe(path):
     return send_from_directory('./iframe_figures/leaf/',path)
 
-
-
+@flaskServer.route('/tiles/<string:s>/<string:z>/<string:x>/<string:y>.png')
+def tileServe(s,z,x,y):
+    print("Tile serve")
+    req = urllib.request.Request(
+    url="https://"+s+".tile.openstreetmap.org/"+z+"/"+x+"/"+y+".png", 
+    data=None, 
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+    }
+    )
+    print("https://"+s+".tile.openstreetmap.org/"+z+"/"+x+"/"+y+".png")
+    return Response(urllib.request.urlopen(req).read(), mimetype='image/png')
+@flaskServer.after_request
+def add_header(response):
+    response.cache_control.max_age = 86400
+    response.cache_control.public=True
+    response.cache_control.immutable=True
+    return response
 
 # window =webview.create_window('Simulations', url="./iframe_figures/figure_0.html",width=900,height=900)
 window =webview.create_window('Simulations', flaskServer,width=900,height=900)
@@ -67,4 +87,9 @@ def evaluate_js(window):
 # r.start()
 
 # webview.start(evaluate_js,window,debug=True, http_server=True,gui="qt")
-webview.start(window,debug=True,gui="qt")
+# webview.start(window,debug=True,gui="qt")
+# input("a")
+if __name__ == '__main__':
+    flaskServer.config['PROPAGATE_EXCEPTIONS'] = True
+    flaskServer.config['TESTING'] = True
+    flaskServer.run(host='0.0.0.0', port=80,debug=True,threaded=False,processes=1)
