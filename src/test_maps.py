40d2851bdbd43bc9713187ad8abc279b67d47ead
Zak Kiernander <kiernazak@ecs.vuw.ac.nz>
2020-09-07 09:49:31 +1200

    Disable maps tests due to CI issues

--- src/test_maps.py
+++ src/test_maps.py
@@ -1,3 +1,6 @@
+#Depreciated due to unable to test on our CI worker
+# Tests of this nature/style would be used if we are able to get the maps to run in a healdess mode on the workers
+
 import pytest
 import subprocess
 import pandas as pd
@@ -20,8 +23,6 @@ def test_cords_passed_correctly():
     for x in range(0,len(mapOut.Latitude)):
         assert np.format_float_positional(mapOut.Latitude[x],precision=7,trim="k",unique=False) == np.format_float_positional(df.Latitude[x],precision=7,trim="k",unique=False)
         # pytest test_maps.py -s -rP
-        # print(np.format_float_positional(mapOut.Latitude[x]) +":"+np.format_float_positional(df.Latitude[x]))
-        # print(np.format_float_positional(mapOut.Longitude[x]) +":"+np.format_float_positional(df.Longitude[x]))
         assert np.format_float_positional(mapOut.Longitude[x],precision=7,trim="k",unique=False) == np.format_float_positional(df.Longitude[x],precision=7,trim="k",unique=False)
 
     print("The exit code was: %d" % mapRunner.returncode)
