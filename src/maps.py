e2cc9285fdecbdb0d1534912af7d58ec69fad7de
goodgecait <goodgecait@ecs.vuw.ac.nz>
2020-09-21 12:26:17 +1200

    Added documentation comments for maps.py

--- src/maps.py
+++ src/maps.py
@@ -12,16 +12,19 @@ class Mapping():
         from flask import Flask,send_from_directory,Response
         flaskServer = Flask(__name__)
 
+        #Read in the file that the simulation data was written to
         df = pd.read_csv(out)
         df.head()
 
         f = open("./iframe_figures/figure_0.html", "w")
 
         markers=""
-
+        
+        #Get all the landing points as markers so that they can be plotted on the map
         for index, row in df.iterrows():
             markers+=("(L.marker([{lat},{long}]).addTo(map))"+("._icon.classList.add('huechange')" if row["Parachute failed"] else "")+";").format(long=row["Longitude"],lat=row["Latitude"])
             
+        
         base="""
         <style>
         img.huechange { filter: hue-rotate(120deg); }
@@ -57,6 +60,7 @@ class Mapping():
         @flaskServer.route('/tiles/<string:s>/<string:z>/<string:x>/<string:y>.png')
         def tileServe(s,z,x,y):
             print("Tile serve")
+            #Get all the Tiles from openstreetview to form the map and save
             filename=s+"-"+z+"-"+x+"-"+y+".png"
             filePath=os.path.join(os.getcwd(),"iframe_figures","tilecache",filename)
             if os.path.exists(filePath):
@@ -77,6 +81,7 @@ class Mapping():
                 print("https://"+s+".tile.openstreetmap.org/"+z+"/"+x+"/"+y+".png")
                 return Response(imgBuf, mimetype='image/png')
 
+        #Draw the map
         window =webview.create_window('Simulations', flaskServer,width=900,height=900)
         def evaluate_js(window):
             result = window.evaluate_js(
